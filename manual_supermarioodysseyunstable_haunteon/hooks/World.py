19357b1dd4852d6751c5bd04d5e2fbc438e7085b
Haunteon <coalmeh@gmail.com>
2026-03-02 23:32:40 -0500

    Fixing version again

--- manual_supermarioodysseyunstable_haunteon/hooks/World.py
+++ manual_supermarioodysseyunstable_haunteon/hooks/World.py
@@ -46,6 +46,7 @@ def before_generate_early(world: World, multiworld: MultiWorld, player: int) -> 
     if get_option_value(multiworld, player, "goal") == 0: #Festival
         multiworld.worlds[player].options.post_metro_moons.value = 1
         multiworld.worlds[player].options.post_peace_moons.value = 0
+        multiworld.worlds[player].options.moon_cave_skip.value = 0
         multiworld.worlds[player].options.post_game_moons.value = 0
         multiworld.worlds[player].options.moon_rock_moons.value = 0
         multiworld.worlds[player].options.achievement_moons.value = 0
@@ -100,6 +101,8 @@ def after_create_regions(world: World, multiworld: MultiWorld, player: int):
 #       will create 5 items that are the "useful trap" class
 # {"Item Name": {ItemClassification.useful: 5}} <- You can also use the classification directly
 def before_create_items_all(item_config: dict[str, int|dict], world: World, multiworld: MultiWorld, player: int) -> dict[str, int|dict]:
+    if get_option_value(multiworld, player, "goal") == 0: #Festival
+        {"Power Moon": 150}
     return item_config
 
 # The item pool before starting items are processed, in case you want to see the raw item pool at that stage
