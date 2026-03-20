837381b5b9b66e346edbb653f9134a45e83d1b37
Haunteon <coalmeh@gmail.com>
2026-03-20 00:18:41 -0400

    Updated both Manuals to Stable Release

--- manual_supermarioodyssey_haunteon/hooks/Rules.py
+++ manual_supermarioodyssey_haunteon/hooks/Rules.py
@@ -100,13 +100,13 @@ def BeatBowser(world: World, multiworld: MultiWorld, state: CollectionState, pla
 
 def EnterDark(multiworld: MultiWorld,state: CollectionState, player: int):
     if get_option_value(multiworld, player, "goal") >= 3: #Darker Side or All Moons
-        return True
-    return "{ItemValue(Darker:250)}"
+        return "{ItemValue(Darker:250)}"
+    return ""
     
 def EnterDarker(multiworld: MultiWorld,state: CollectionState, player: int):
     if get_option_value(multiworld, player, "goal") >= 3: #Darker Side or All Moons
-        return True
-    return "{ItemValue(Darker:500)}"
+        return "{ItemValue(Darker:500)}"
+    return ""
     
 # def DarkerEnabled(multiworld: MultiWorld,state: CollectionState, player: int, valueCount: str):
 #    if get_option_value(multiworld, player, "goal") >= 3: #Darker Side or All Moons
